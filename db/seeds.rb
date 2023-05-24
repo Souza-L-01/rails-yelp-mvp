eed8182ec5c1091f1a34eb5efc7e3bb5af3cd263
Lucas_Souza <souza.vilela.lucas@gmail.com>
2023-05-24 16:39:04 +0200

    all good

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create!(
+    name: "Restaurant 1",
+    address: "Address 1",
+    phone_number: "123",
+    category: "chinese"
+)
+  
+Restaurant.create!(
+    name: "Restaurant 2",
+    address: "Address 2",
+    phone_number: "123",
+    category: "italian"
+)
+
+Restaurant.create!(
+    name: "Restaurant 3",
+    address: "Address 3",
+    phone_number: "123",
+    category: "french"
+)
+
+Restaurant.create!(
+    name: "Restaurant 4",
+    address: "Address 4",
+    phone_number: "123",
+    category: "belgian"
+)
+
+Restaurant.create!(
+    name: "Restaurant 5",
+    address: "Address 5",
+    phone_number: "123",
+    category: "japanese"
+)
+  
+  # Add more restaurant records as needed
+  
